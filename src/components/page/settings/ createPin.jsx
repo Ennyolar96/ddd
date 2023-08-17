84ac8fe6a86fd562fde1b68c77adf439c30b0849
Olaniyan Mutiu <63626592+Ennyolar96@users.noreply.github.com>
2023-08-17 17:56:46 +0100

    add new file

---
+++ src/components/page/settings/ createPin.jsx
@@ -0,0 +1,14 @@
+export default function CreatePin() {
+    return (
+        <>
+
+            <div className="row row-cols-md-2 row-cols-sm-2 row-cols-1">
+                <div className="col">
+                    <div className="">
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
